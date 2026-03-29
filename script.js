0cbbba5e8cc34263d91d0c3d728f38251bf513d8
ananyasreeb <ananya121224@gmail.com>
2026-03-29 19:08:50 +0300

    Update script.js

--- script.js
+++ script.js
@@ -121,4 +121,16 @@ if ("serviceWorker" in navigator) {
 }
 function toggleTheme() {
   document.body.classList.toggle("dark");
+
+  if (document.body.classList.contains("dark")) {
+    localStorage.setItem("theme", "dark");
+  } else {
+    localStorage.setItem("theme", "light");
+  }
+}
+
+/* LOAD SAVED THEME */
+if (localStorage.getItem("theme") === "dark") {
+  document.body.classList.add("dark");
+}
 }
